ad2b13dff14fbb7a9a4b137f84af87c1681ef33c
Administrator <qskeksq@gmail.com>
2017-09-06 18:40:06 +0900

    board

--- Board.java
+++ Board.java
@@ -1,82 +1,104 @@
-package com.nadan.java.arrayboard;
+package ArrayBoard;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Board {
 	
+	int[] scores = new int[0];  //학생 점수를 저장할 배열
+	Scanner scanner;
+	boolean run = true; 
+	String select;
+	int sum;  //전체 학생 점수 분석을 위한 총 값
+	double avg;  //평균값
+	
 	//점수 관리 프로그램
 	public void board(){
 		
-		Scanner scanner = new Scanner(System.in); //스캐너로 문자열 입력받는다
-		boolean run = true; 
-		int[] scores = new int[0];  //학생 점수를 저장할 배열
-		int sum = 0;  //전체 학생 점수 분석을 위한 총 값
-		double avg = 0;  //평균값
+		scanner = new Scanner(System.in); //스캐너로 문자열 입력받는다
+		sum = 0;
+		avg = 0;
 		
 		while(run){
 			System.out.println("-------------------------------------------");
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
 			System.out.println("-------------------------------------------");
 			
-			String select = scanner.nextLine();
+			select = scanner.nextLine();
 			
 			//원하는 학생수 넣어주기
 			if(select.equals("1")){
-				System.out.println("선택>"+select);
-				System.out.print("학생수 입력>");
-				String num = scanner.nextLine();
-				//입력받은 학생수를 int값으로 변환, int[]배열의 길이로 지정해 준다
-				scores = new int[Integer.parseInt(num)];
-				System.out.println("학생수 확인>"+num); //입력받은 학생 수 확인
-				System.out.println();
+				inputStudentNo();
 			//성적 입력
 			} else if(select.equals("2")){
-				System.out.println("선택>"+select);
-				//배열에 들어간 학생 수만큼 학생을 출력하고, 각각 점수를 부여한다. 점수는 scores 배열에 저장된다.
-				int length = scores.length; //성적 배열로부터 학생 수를 알 수 있다.
-				for(int i=0; i<length; i++){
-					System.out.print("student["+(i+1)+"] : ");
-					String score1 = scanner.nextLine();
-					scores[i] = Integer.parseInt(score1); //입력받은 점수를 배열에 저장
-				}
-				System.out.println();
+				inputGrade();
 			//성적 리스트 확인
 			} else if(select.equals("3")){
-				System.out.println("선택>"+select);
-				int length = scores.length;
-				for(int i=0; i<length; i++){
-					System.out.println("student["+(i+1)+"] : "+scores[i]);
-				}
-				System.out.println();
-			
+				checkGrade();
 			//성적 분석
 			} else if(select.equals("4")){
-				System.out.println("선택>"+select);
-				Arrays.sort(scores); //sort 메소드를 통해 점수를 정렬하고, 마지막 항을 출력한다.
-				int length = scores.length; //마지막 항-전체 길이에서 -1 해 준 값을 넣어준 값
-				System.out.println("최고 점수 : "+scores[length-1]);
-				//향상된 for문을통해 scores배열에 들어간 모든 값을 더해준다.
-				for(int a : scores){
-					sum = sum + a;
-				}
-				avg = (double)sum/length;  //평균값
-				System.out.println("평균 점수 : "+avg);
-				System.out.println();
-			
+				analyze();
 			//프로그램 종료
 			} else if(select.equals("5")){
-				System.out.println("선택>"+select);
-				System.out.println("프로그램을 종료합니다");
-				run = false; 
-				
+				exit();
 			}
-			
 		}
 		scanner.close();
-		
-		
-		
 	}
-
+	
+	// 학생 수 입력
+	private void inputStudentNo() {
+		System.out.println("선택>"+select);
+		System.out.print("학생수 입력>");
+		String num = scanner.nextLine();
+		//입력받은 학생수를 int값으로 변환, int[]배열의 길이로 지정해 준다
+		scores = new int[Integer.parseInt(num)];
+		System.out.println("학생수 확인>"+num); //입력받은 학생 수 확인
+		System.out.println();
+	}
+	
+	// 성적 입력
+	private void inputGrade() {
+		System.out.println("선택>"+select);
+		//배열에 들어간 학생 수만큼 학생을 출력하고, 각각 점수를 부여한다. 점수는 scores 배열에 저장된다.
+		int length = scores.length; //성적 배열로부터 학생 수를 알 수 있다.
+		for(int i=0; i<length; i++){
+			System.out.print("student["+(i+1)+"] : ");
+			String score1 = scanner.nextLine();
+			scores[i] = Integer.parseInt(score1); //입력받은 점수를 배열에 저장
+		}
+		System.out.println();
+	}
+	
+	//성적 확인
+	private void checkGrade() {
+		System.out.println("선택>"+select);
+		int length = scores.length;
+		for(int i=0; i<length; i++){
+			System.out.println("student["+(i+1)+"] : "+scores[i]);
+		}
+		System.out.println();
+	}
+	
+	// 분석
+	private void analyze() {
+		System.out.println("선택>"+select);
+		Arrays.sort(scores); //sort 메소드를 통해 점수를 정렬하고, 마지막 항을 출력한다.
+		int length = scores.length; //마지막 항-전체 길이에서 -1 해 준 값을 넣어준 값
+		System.out.println("최고 점수 : "+scores[length-1]);
+		//향상된 for문을통해 scores배열에 들어간 모든 값을 더해준다.
+		for(int a : scores){
+			sum = sum + a;
+		}
+		avg = (double)sum/length;  //평균값
+		System.out.println("평균 점수 : "+avg);
+		System.out.println();
+	}
+	
+	// 종료
+	private void exit() {
+		System.out.println("선택>"+select);
+		System.out.println("프로그램을 종료합니다");
+		run = false; 
+	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,111 @@
 # ArrayBoard
 
-- while문은 자칫하면 무한으로 순환해서 리소스를 낭비하기 쉬운데, 순환문 내에서 적절히 조절해주면 할 일을 많이 줄여는 것의 좋은 예
-- 임시 저장소를 이용해 데이터 저장하기, 불러오기의 이해
-- Arrays.sort를 통해 최고값 리턴하는 좋은 예
+- while, Scanner 이용 값 입력
+- 객체 지향 함수 분리
+- 임시 저장(Runtime Data)
+- Arrays.sort 이용 최고값 리턴
+
+
+```java
+
+public void board(){
+
+	scanner = new Scanner(System.in); //스캐너로 문자열 입력받는다
+	sum = 0;
+	avg = 0;
+
+    while(run){
+    	System.out.println("-------------------------------------------");
+    	System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
+    	System.out.println("-------------------------------------------");
+
+    	select = scanner.nextLine();
+
+    	//원하는 학생수 넣어주기
+    	if(select.equals("1")){
+    		inputStudentNo();
+    	//성적 입력
+    	} else if(select.equals("2")){
+    		inputGrade();
+    	//성적 리스트 확인
+    	} else if(select.equals("3")){
+    		checkGrade();
+    	//성적 분석
+    	} else if(select.equals("4")){
+    		analyze();
+    	//프로그램 종료
+    	} else if(select.equals("5")){
+    		exit();
+    	}
+    }
+```
+>학생 수 입력
+
+```java
+private void inputStudentNo() {
+	System.out.println("선택>"+select);
+	System.out.print("학생수 입력>");
+	String num = scanner.nextLine();
+	//입력받은 학생수를 int값으로 변환, int[]배열의 길이로 지정해 준다
+	scores = new int[Integer.parseInt(num)];
+	System.out.println("학생수 확인>"+num); //입력받은 학생 수 확인
+	System.out.println();
+}
+```
+
+>// 성적 입력
+
+```java
+private void inputGrade() {
+	System.out.println("선택>"+select);
+	//배열에 들어간 학생 수만큼 학생을 출력하고, 각각 점수를 부여한다. 점수는 scores 배열에 저장된다.
+	int length = scores.length; //성적 배열로부터 학생 수를 알 수 있다.
+	for(int i=0; i<length; i++){
+		System.out.print("student["+(i+1)+"] : ");
+		String score1 = scanner.nextLine();
+		scores[i] = Integer.parseInt(score1); //입력받은 점수를 배열에 저장
+	}
+	System.out.println();
+}
+```
+
+>성적 확인
+
+```java
+private void checkGrade() {
+	System.out.println("선택>"+select);
+	int length = scores.length;
+	for(int i=0; i<length; i++){
+		System.out.println("student["+(i+1)+"] : "+scores[i]);
+	}
+	System.out.println();
+}
+```
+
+>분석
+
+```java
+private void analyze() {
+	System.out.println("선택>"+select);
+	Arrays.sort(scores); //sort 메소드를 통해 점수를 정렬하고, 마지막 항을 출력한다.
+	int length = scores.length; //마지막 항-전체 길이에서 -1 해 준 값을 넣어준 값
+	System.out.println("최고 점수 : "+scores[length-1]);
+	//향상된 for문을통해 scores배열에 들어간 모든 값을 더해준다.
+	for(int a : scores){
+		sum = sum + a;
+	}
+	avg = (double)sum/length;  //평균값
+	System.out.println("평균 점수 : "+avg);
+	System.out.println();
+}
+```
+
+>종료
+
+```java
+private void exit() {
+	System.out.println("선택>"+select);
+	System.out.println("프로그램을 종료합니다");
+	run = false;
+}
+```
